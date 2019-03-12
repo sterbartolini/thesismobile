539e37012a0275d203ef7bcd9d26b5a3c433620f
sterbartolini <lesterbartolini@gmail.com>
2019-03-13 02:44:27 +0800

    map with markers (geolocation)

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,57 @@
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Platform, Alert, } from 'react-native';
 
 
 class Map extends Component {
+    state = {
+        location: 0,
+        lat: 0,
+        lng: 0,
+    };
+
+    findCoordinates = () => {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                const lat = JSON.stringify(position.coords.latitude);
+                const lng = JSON.stringify(position.coords.longitude);
+
+                this.setState({ lat, lng });
+            },
+            error => Alert.alert(error.message),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        );
+    };
+
     render() {
         return (
             <View style={styles.container}>
-                <MapView
+                {/* <MapView
                     provider={PROVIDER_GOOGLE} // remove if not using Google Maps
                     style={styles.map}
-                    region={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
+                    initialRegion={{
+                        latitude: this.state.lat,
+                        longitude: this.state.lng,
                         latitudeDelta: 0.015,
                         longitudeDelta: 0.0121,
                     }}
                 >
 
                     <MapView.Marker
+
                         coordinate={{
-                            latitude: 37.78825,
-                            longitude: -122.4324,
+                            latitude: this.state.lat,
+                            longitude: this.state.lng,
                         }}
                     //   description={marker.description}
                     />
 
-                </MapView>
+                </MapView> */}
+                <TouchableOpacity onPress={this.findCoordinates}>
+                    <Text>Find My Coords?</Text>
+                    <Text>Latitude: {this.state.lat}</Text>
+                    <Text>longitude: {this.state.lng}</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -42,6 +67,11 @@ const styles = StyleSheet.create({
     },
     map: {
         ...StyleSheet.absoluteFillObject,
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10
     },
 });
 
